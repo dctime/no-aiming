d73954be69f551c93c0111581e1099144e83514e
DCtime <daniel.chu916@gmail.com>
2020-12-13 00:29:46 +0800

    Added Score Counting and UI
    scores are important

---
+++ NoAiming/Assets/Scripts/PlayerScoreManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerScoreManager : MonoBehaviour
+{
+    //儲存玩家點數
+    private int score = 0;
+    //紀錄得點數間隔
+    private float time;
+
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "ScoreEffective")
+        {
+            if (time >= 1)
+            {
+                Debug.Log(gameObject.name + " added 1 point");
+                score += 1;
+                time = 0;
+            }
+            
+            time += Time.deltaTime;
+
+        }
+        else
+        {
+            time = 0;
+        }
+
+        if (score > 100)
+        {
+            score = 100;
+        }
+    }
+
+    public int GetScore()
+    {
+        return score;
+    }
+}
---
+++ NoAiming/Assets/Scripts/ScoreBarScoreHandler.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreBarScoreHandler : MonoBehaviour
+{
+    [Header("點數條")]
+    [SerializeField] private Image scoreBar;
+
+    [Header("目標玩家物件")]
+    [SerializeField] private GameObject player;
+
+    private void Update()
+    {
+        scoreBar.fillAmount = player.GetComponent<PlayerScoreManager>().GetScore() / 100f;
+    }
+}
