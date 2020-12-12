ee7904ddce4f939fcbc076876bc7404a0e5a1a53
DCtime <daniel.chu916@gmail.com>
2020-12-13 00:38:50 +0800

    Added Timer
    time adds salt to the vegetable

---
+++ NoAiming/Assets/Scripts/CountDownTimerTimeCounter.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CountDownTimerTimeCounter : MonoBehaviour
+{
+    [Header("從第幾秒開始倒數計時?")]
+    [SerializeField] private float time;
+
+    private void Update()
+    {
+        time -= Time.deltaTime;
+
+        GetComponent<Text>().text = ((int) time).ToString();
+    }
+}
