2a86bc63ddd09cf44847d5fd9f7e9244a4cdb605
DCtime <daniel.chu916@gmail.com>
2020-12-20 13:38:30 +0800

    NoAiming v1.2 Finished
    HowToPlayScene Complete
    A lot of Buttons
    Full game flow done

---
+++ NoAiming/Assets/Scripts/Prefab/ExitButton/ExitButtonHandler.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExitButtonHandler : MonoBehaviour
+{
+    public void ButtonDown()
+    {
+        Application.Quit();
+        UnityEditor.EditorApplication.isPlaying = false;
+    }
+}
---
+++ NoAiming/Assets/Scripts/StartMenu/HowToPlayButtonHandler.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class HowToPlayButtonHandler : MonoBehaviour
+{
+    public void ButtonDown()
+    {
+        SceneManager.LoadScene("HowToPlayScene");
+    }
+}
