6a814d30d460e4b264dd31c0f5f2d3d93076c164
DCtime <daniel.chu916@gmail.com>
2021-01-28 23:40:17 +0800

    1.3.1 Sniper Rifle Has Arrived
    added cool down (5 seconds)
    when hold the fire button,it will show an indicator which refer to the guns accuracy.
    The more u hold, the more accurate it will be.

---
+++ NoAiming/Assets/Scripts/Prefab/SniperRifle/Gun/SniperRifleFire.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SniperRifleFire : MonoBehaviour
+{
+    //儲存發射鍵
+    private KeyCode fireKey;
+
+    [Header("範圍指示器")]
+    [SerializeField] GameObject aimIndicator;
+
+    [Header("範圍指示器 - 模型")]
+    [SerializeField] GameObject aimIndicatorModel;
+
+    [Header("子彈Prefab")]
+    [SerializeField] private GameObject bulletType;
+
+    [Header("槍口")]
+    [SerializeField] private GameObject muzzle;
+
+    [Header("槍枝參數")]
+    [SerializeField] private float fireRate;
+    [SerializeField] private int damage;
+
+    //儲存最近發射的子彈
+    private GameObject currentBullet;
+
+    [Header("敵方物件")]
+    [SerializeField] private GameObject emenyObject;
+
+    [Header("開火音效")]
+    [SerializeField] AudioSource fireSoundEffect;
+
+    //儲存已冷卻秒數
+    private float coolDownTimer;
+
+    //儲存是否可以瞄準
+    private bool canAim;
+
+    //儲存是否正在瞄準
+    private bool isAiming;
+
+    private void Start()
+    {
+        //初始化fireKey
+        fireKey = gameObject.transform.parent.gameObject.GetComponent<PlayerFire>().fireKey;
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown(fireKey) && canAim)
+        {
+            //開啟瞄準
+            aimIndicator.SetActive(true);
+            //將isAiming設為true，供發射子彈時參考
+            isAiming = true;
+        }
+
+        if (Input.GetKeyUp(fireKey) && isAiming && canAim)
+        {
+            aimIndicator.SetActive(false);
+            //產生子彈，並放到muzzle
+            currentBullet = Instantiate(bulletType, muzzle.transform.position, transform.rotation);
+            // --- 將產生出來的物件之元件加入一些參數 ---
+            //給SniperRifleBulletMovement敵方物件
+            currentBullet.GetComponent<SniperRifleBulletMovement>().emenyObject = emenyObject;
+            //給SniperRifleBulletCollider敵方物件與傷害
+            currentBullet.GetComponent<SniperRifleBulletCollider>().emenyObject = emenyObject;
+            currentBullet.GetComponent<SniperRifleBulletCollider>().damage = damage;
+            //給SniperRifleBulletMovement偏差角度
+            currentBullet.GetComponent<SniperRifleBulletMovement>().angularMisalignment = aimIndicatorModel.GetComponent<SniperAimAreaIndicator>().AimIndicatorAngle_Getter();
+            //播放音效
+            fireSoundEffect.Play();
+            //開始冷卻
+            coolDownTimer = 0;
+            canAim = false;
+            //將isAiming設為false
+            isAiming = false;
+        }
+
+        //當冷卻未完成時，冷卻
+        if (coolDownTimer < fireRate && !canAim)
+        {
+            coolDownTimer += Time.deltaTime;
+        }
+        else //當冷卻已完成時，允許瞄準
+        {
+            canAim = true;
+        }
+
+        
+        
+    }
+}
