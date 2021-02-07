d61a79307816df7c936cde7a1bc04f8def66ac5b
DCtime <daniel.chu916@gmail.com>
2021-02-08 00:58:20 +0800

    Scene Build, Target and Button, Character Name Display
    Thats a lot of work :|

---
+++ NoAiming/Assets/Scripts/SelectCharacterScene/CharacterNameController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CharacterNameController : MonoBehaviour
+{
+    [Header("角色群體")]
+    [SerializeField] GameObject characterGroup;
+
+    [Header("目標文字")]
+    [SerializeField] Text text;
+
+    private void Update()
+    {
+        for (int i = 0; i < characterGroup.transform.childCount; i++)
+        {
+            if (characterGroup.transform.GetChild(i).gameObject.activeSelf == true)
+            {
+                text.text = characterGroup.transform.GetChild(i).gameObject.name;
+                break;
+            }
+        }
+        
+    }
+}
--- NoAiming/Assets/Scripts/SelectCharacterScene/SelectCharacterController.cs
+++ NoAiming/Assets/Scripts/SelectCharacterScene/SelectCharacterController.cs
@@ -5,105 +5,80 @@ using UnityEngine;
 
 public class SelectCharacterController : MonoBehaviour
 {
-    private enum Character
-    {
-        PewPewGunner,
-        Sniper
-    }
+    [Header("角色群體")]
+    [SerializeField] private GameObject characterGroup;
+    
+    //角色陣列
+    private GameObject[] characters;
 
-    Character_Linked_Array characters;
+    //現在顯示在螢幕上的角色物件和索引
+    private GameObject currentCharacter;
+    private int currentIndex;
 
-    class Node
-    {
-
-        private Character character;
-        private Node next;
-        private Node previous;
-
-        public Node(Character character)
-        {
-            this.character = character;
-        }
-
-        public Node Next_Getter()
-        {
-            return next;
-        }
-        public void Next_Setter(Node next_node)
-        {
-            this.next = next_node;
-        }
-        public Node Previous_Getter()
-        {
-            return previous;
-        }
-        public void Previous_Setter(Node previous_node)
-        {
-            previous = previous_node;
-        }
-        public Character Character_Getter()
-        {
-            return character;
-        }
-
-    }
-
-    class Character_Linked_Array
-    {
-        //儲存此鏈結串列的開頭節點
-        private Node head;
-        private Node end;
-
-        //添加一個新節點
-        public void Add_Node(Node node)
-        {
-            //如果這個鏈結陣列沒有頭
-            if (head == null)
-            {
-                head = node;
-                end = node;
-            }
-            else //如果這個鏈結陣列有頭
-            {
-                end.Next_Setter(node);
-                node.Previous_Setter(end);
-                end = node;
-            }
-
-        }
-
-        public void PrintArray()
-        {
-            Node nowPrinting;
-
-            nowPrinting = head;
-
-            while (true)
-            {
-                if (nowPrinting == null)
-                {
-                    Debug.Log("No more");
-                    break;
-                }
-                else
-                {
-                    Debug.Log(nowPrinting.Character_Getter());
-                    nowPrinting = nowPrinting.Next_Getter();
-                }
-            }
-        }
-    }
+    [Header("按鍵設定")]
+    [SerializeField] private KeyCode nextKey;
+    [SerializeField] private KeyCode previousKey;
 
     private void Start()
     {
-        //做一個Node, 存放PewPewGunner的資料
-        Node pewPewGunner = new Node(Character.PewPewGunner);
-        Node sniper = new Node(Character.Sniper);
+        //將角色群體的角色搬到characters陣列裡
+        characters = new GameObject[characterGroup.transform.childCount];
 
-        characters = new Character_Linked_Array();
+        for (int i = 0; i < characterGroup.transform.childCount; i++)
+        {
+            characters[i] = characterGroup.transform.GetChild(i).gameObject;
+        }
+        
+        //將角色陣列的第一個角色設為目前顯示在場景上的角色
+        currentIndex = 0;
+        currentCharacter = characters[currentIndex];
+        characters[0].SetActive(true);
+    }
 
-        characters.Add_Node(pewPewGunner);
-        characters.Add_Node(sniper);
-        characters.PrintArray();
+    private void Update()
+    {
+        //如果按下會切換成下一個角色的按鈕
+        if (Input.GetKeyDown(nextKey))
+        {
+            //目前索引加一
+            currentIndex += 1;
+            
+            //如果目前索引已大於此陣列最大索引
+            if (currentIndex + 1 > characters.Length)
+            {
+                //將其歸零
+                currentIndex = 0;
+            }
+
+            //將原本的角色關掉
+            currentCharacter.SetActive(false);
+            //將下一個角色的物件存入currentCharacter
+            currentCharacter = characters[currentIndex];
+            //將下一個物件激活
+            currentCharacter.SetActive(true);
+            //將下一個物件的位置歸零
+            currentCharacter.transform.localPosition = new Vector3(0, 0, 0);
+        }
+        else if (Input.GetKeyDown(previousKey)) //如果按下會切換成上一個角色的按鈕
+        {
+            //目前索引減一
+            currentIndex -= 1;
+
+            //如果目前索引小於零
+            if (currentIndex < 0)
+            {
+                //將其設為最大值
+                currentIndex = characters.Length - 1;
+            }
+
+            //將原本的角色關掉
+            currentCharacter.SetActive(false);
+            //將上一個角色的物件存入currentCharacter
+            currentCharacter = characters[currentIndex];
+            //將上一個物件激活
+            currentCharacter.SetActive(true);
+            //將上一個物件的位置歸零
+            currentCharacter.transform.localPosition = new Vector3(0, 0, 0);
+        }
     }
 }
