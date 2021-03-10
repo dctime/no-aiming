9281236603616babcd19d373d79fcad6cfb1de03
DCtime <daniel.chu916@gmail.com>
2021-03-10 13:02:42 +0800

    Added PlayerAIMovement

---
+++ NoAiming/Assets/Scripts/Prefab/Player/PlayerAIMovement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAIMovement : MonoBehaviour
+{
+    [Header("手動控制移動腳本")]
+    [SerializeField] private GameObject PlayerMovementScript;
+
+    [Header("敵方玩家")]
+    [SerializeField] private GameObject EmenyObject;
+
+    private void Update()
+    {
+        //如果AI自動控制開啟，手動控制還是保持開啟，提出警告
+        if (PlayerMovementScript.activeSelf == true)
+        {
+            Debug.Log("WARNING : Both Modes Are Activated!");
+        }
+    }
+}
